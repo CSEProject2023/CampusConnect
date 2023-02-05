d3b4081cf683b8c988edfd0a5f7d478e81f09090
Sailaja <sailajapbs88@gmail.com>
2023-02-05 23:01:21 +0530

    bala_app.js

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+var express = require('express')
+var mysql = require("mysql")
+var app = new express();
+app.use(express.static(__dirname + '/static'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/static/home.html')
+})
+app.get('/student_signup',function (req, res) {
+  res.sendFile(__dirname+'/static/student_signup.html')
+})
+app.listen(3000);
