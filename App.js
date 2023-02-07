a314d7230717c45e55629098f5c83d55097cd7c1
Sailaja <sailajapbs88@gmail.com>
2023-02-07 21:17:19 +0530

    js basic changes

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 var express = require('express')
 var mysql = require("mysql")
+var path = require('path')
 var app = new express();
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/static/home.html')
 })
 app.get('/student_signup',function (req, res) {
   res.sendFile(__dirname+'/static/student_signup.html')
 })
+app.get('/admin_signup',function (req, res) {
+  res.sendFile(__dirname+'/static/admin_signup.html')
+})
+app.get('/admin_login',function (req, res) {
+  res.sendFile(__dirname+'/static/admin_login.html')
+})
+app.get('/student_login',function (req, res) {
+  res.sendFile(__dirname+'/static/student_login.html')
+})
+
+// DB connection
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 app.listen(3000);
